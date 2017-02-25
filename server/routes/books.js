e9cc9b95a0a769c70a7b0f9ac628e01837ed147b
kkh0811 <gkim16@my.centennialcollege.ca>
2017-02-25 10:14:26 -0500

    added inline comments

--- server/routes/books.js
+++ server/routes/books.js
@@ -33,25 +33,25 @@ router.get('/', (req, res, next) => {
 
 //  GET the Book Details page in order to add a new Book
 router.get('/add', (req, res, next) => {
-  res.render('books/details', {
-    title: "Add a new Book",
-    books: ''
+  res.render('books/details', { // to direct details page.
+    title: "Add a new Book", // add title for the page.
+    books: '' // should be null for default page
   });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
 router.post('/add', (req, res, next) => {
-  book.create({
-    "Title": req.body.title,
+  book.create({ 
+    "Title": req.body.title, // to bring data from textfield
     "Price": req.body.price,
     "Author": req.body.author,
     "Genre": req.body.genre
   }, (err, book) => {
-    if(err) {
+    if(err) { //if occurs error, shows the error on the consle
       console.log(err);
       res.end(err);
     } else {
-      res.redirect('/books');
+      res.redirect('/books'); // if no error, redirect to books page.
     }
   });
 });
@@ -59,9 +59,9 @@ router.post('/add', (req, res, next) => {
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
 
-    let id = req.params.id;
+    let id = req.params.id; // search book id 
 
-    book.findById(id, (err, books) => {
+    book.findById(id, (err, books) => { // come up with bookId from html
       if(err) {
       console.log(err);
       res.end(err);
@@ -80,13 +80,13 @@ router.post('/:id', (req, res, next) => {
     let id = req.params.id;
     let books = new book({
       "_id": id,
-      "Title": req.body.title,
+      "Title": req.body.title, // to get data from textfield
       "Price": req.body.price,
       "Author": req.body.author,
       "Genre": req.body.genre
     });
 
-    book.update({ _id: id}, books, (err) => {
+    book.update({ _id: id}, books, (err) => { // update book based on book id
       if(err) {
         console.log(err);
         res.end(error);
@@ -101,7 +101,7 @@ router.post('/:id', (req, res, next) => {
 router.get('/delete/:id', (req, res, next) => {
   let id = req.params.id;
 
-  book.remove({_id: id}, (err) => {
+  book.remove({_id: id}, (err) => { // delete book based on book id
     if(err) {
       console.log(err);
       res.end(err);
