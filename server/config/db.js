a15132bcb23c7138727fd7975ca0847058749c51
kkh0811 <gkim16@my.centennialcollege.ca>
2017-02-25 11:34:32 -0500

    test on mLab

--- server/config/db.js
+++ server/config/db.js
@@ -9,6 +9,7 @@ Web App name: COMP308-W2017-MIDTERM
 
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books308"
+  //"URI": "mongodb://localhost/books308"
   //Remote MongoDB deployment -> let URI = "mongodb://kkh0811:12345@ds058369.mlab.com:58369/books308";
+  "URI" : "mongodb://kkh0811:12345@ds058369.mlab.com:58369/books308"
 };
